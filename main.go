08cb58729f095e78a88a2ae4b0f8dd992c74dcd3
Mario <mariopuebla@gmail.com>
2023-05-14 14:12:10 -0300

    Primer commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/maalpu/zeiboxuser/awsgo"
+)
+
+func main() {
+	lambda.Start(EjecutoLambda)
+}
+
+func EjecutoLambda(ctx context.Context, event events.CognitoEventUserPoolsPostConfirmation) (events.CognitoEventUserPoolsPostConfirmation, error) {
+	awsgo.InicializoAWS()
+}
